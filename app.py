fddf69d22db43c2c357d8b10732d6b4b57daae86
tharnadek <tharnadek@gmail.com>
2019-02-28 19:27:09 -0800

    add heartbeat functions

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, Response, send_from_directory
 app = Flask(__name__)
 
+"""
+Check the vitals
+"""
 @app.route('/')
 def route_root():
     return "I'm a snake, and I have good grammar."
+
+"""
+Serve a static file for the snake head image
+"""
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+"""
+This keeps the snake alive, in case of Heroku
+taking an early coffee break.
+"""
+@app.route('/ping')
+def ping():
+    return Response(
+        status=200
+    )
 
